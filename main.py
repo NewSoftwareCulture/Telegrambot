9578815a485a51caae06ba4ec9b0eb7c88ff25c3
Vlad <vlados.pochta@gmail.com>
2020-02-28 02:45:47 +0300

    ,

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ import requests
 import time
 import config
 
-# bot = telebot.TeleBot(config.token)
+bot = telebot.TeleBot(config.token)
 
-# @bot.message_handler(commands=['start'])
-# def start_message(message):
-#     bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
-print('Hello')
-# bot.polling()
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+bot.polling()
