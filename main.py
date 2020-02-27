dc1b2ab97391b9ebe5110bb3928f9c2b074a3b6c
Vlad <vlados.pochta@gmail.com>
2020-02-28 02:27:16 +0300

    test

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import telebot
-import requests
-import time
-import config
+# import telebot
+# import requests
+# import time
+# import config
 
-bot = telebot.TeleBot(config.token)
+# bot = telebot.TeleBot(config.token)
+print("Hello world")
 
+# bot.send_message(config.channel_name, 'hello')
 
-bot.send_message(config.channel_name, 'hello')
-
-bot.polling()
+# bot.polling()
