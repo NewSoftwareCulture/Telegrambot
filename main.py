1022bba0f1b5f3adb1ce0d19a5a69f986fdba96c
Vlad <vlados.pochta@gmail.com>
2020-02-28 03:38:44 +0300

    Test1

--- main.py
+++ main.py
@@ -2,12 +2,33 @@ import telebot
 import requests
 import time
 import config
+import extras
 
 bot = telebot.TeleBot(config.token)
 
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id, 'Я твой бот, привет')
+
+
 # def start_message(message):
     # bot.send_message(message.chat.id, 'Я твой бот, привет')
-bot.polling()
+
+while True:
+    try:
+        @bot.message_handler(commands=['hi','hello',"Hi","Hello"])
+        def start_message(message):
+            bot.send_message(message.chat.id, 'Здарова, я твой бот, бро')
+
+        @bot.message_handler(commands=['action'])
+        def message1(message):
+            bot.send_message(message.chat.id, 'Да я многое могу')
+
+        @bot.message_handler(commands=['finish'])
+        def message2(message):
+            bot.send_message(message.chat.id, 'Пока, чел')
+
+        bot.polling(none_stop=True)
+
+    except Exception as e:
+        print(e)  # или просто print(e) если у вас логгера нет,
+        # или import traceback; traceback.print_exc() для печати полной инфы
+        time.sleep(15)
+    
