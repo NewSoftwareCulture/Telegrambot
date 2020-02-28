40a3333360ba6d5619605354145e9fd24c48fd2a
Vlad <vlados.pochta@gmail.com>
2020-02-28 14:37:21 +0300

    .

--- actions.py
+++ actions.py
@@ -1,18 +1,7 @@
-import telebot
-# import config
-from main import bot
+import config
 
 
-def message1(message):
-    bot.send_message(message.chat.id, 'Лучше бы я пошел с мастером Люком, чем торчал здесь с тобой. Уж не знаю в чём дело, но уверен, что в этом есть твоя вина!')
-            
-def message2(message):
-    if message.text.lower() == 'hi':
-        bot.send_message(message.chat.id, 'Здарова, я твой бот, бро')
-    elif message.text.lower() == 'action':
-        bot.send_message(message.chat.id, 'Да я многое могу')
-    elif message.text.lower() == 'finish':
-        bot.send_message(message.chat.id, 'Пока, чел')
+
     # elif message.text.lower() == 'channel':
     #     bot.send_message(config.channel_name, 'Пишу в канал')
     # elif message.text.lower() == 'chat':
