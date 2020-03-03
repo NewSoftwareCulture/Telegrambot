cb301fc9080f40aff00b2c6669511866685aedb3
Vlad <vlados.pochta@gmail.com>
2020-03-03 16:40:21 +0300

    Add menu command

--- actions/commands.py
+++ actions/commands.py
@@ -4,5 +4,11 @@ def commands():
     @bot.message_handler(
         commands=['start'], 
     )
-    def func(message):
+    def func_start(message):
         bot.send_message(message.chat.id, 'Лучше бы я пошел с мастером Люком, чем торчал здесь с тобой. Уж не знаю в чём дело, но уверен, что в этом есть твоя вина!')
+    
+    @bot.message_handler(
+        commands=['menu'], 
+    )
+    def func_menu(message):
+        bot.send_message(message.chat_id, "Напиши 'ID' для получения своего id\nМожешь написать 'привет' и я поздароваюсь с тобой")
