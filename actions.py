c84170fbd3be3807285fb8321fb28414a7710e7c
Vlad <vlados.pochta@gmail.com>
2020-03-03 15:56:59 +0300

    Heroku test

--- actions.py
+++ actions.py
@@ -13,9 +13,8 @@ def messages():
         content_types=['text'],
     )
     def func(message):
+        print(message.text.lower().find('id'))
         if message.text.lower() == 'привет':
             bot.send_message(message.chat.id, "Привет, " + message.from_user.first_name + ", я тут местный бот")
-        if message.text.lower().find('id'):
-            bot.send_message(message.chat.id, "Твой ID: " + message.chat.id)
-        if message.text.lower().find('ip') > -1:
-            bot.send_message(message.chat.id, "Твой IP: " + message.from_user.ip)
+        if message.text.lower().find('id') > -1:
+            bot.send_message(message.chat.id, "Твой ID: " + str(message.from_user.id))
--- main.py
+++ main.py
@@ -8,12 +8,13 @@ def main():
         try:
             actions.commands()
             actions.messages()
-
+            
             bot.infinity_polling(True) 
 
         except Exception as e:
-            print(e)  # или просто print(e) если у вас логгера нет,
-            bot.send_message(channel_name, e) 
+            error_code = "C3PO_bot! \n\nError: \n" + str(e)
+            print(error_code)  # или просто print(e) если у вас логгера нет,
+            bot.send_message(channel_name, error_code) 
 
 if __name__ == '__main__':
     main()
