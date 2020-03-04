6954b30a2dec82df8efbf78812ef8e865619c49a
Vlad <vlados.pochta@gmail.com>
2020-03-04 22:06:09 +0300

    Update parser weather
    Адекватный вывод погоды с направлением и скоростью ветра

--- actions/get_weather.py
+++ actions/get_weather.py
@@ -16,18 +16,21 @@ def get_temperature(html):
 
 def get_weather(html):
     buffer = html.find('div', id='hero-left-Nowcard-92c6937d-b8c3-4240-b06c-9da9a8b0d22b').find_all(class_='today_nowcard-phrase')
-    weather = re.findall(r'[А-Я,а-я]', str(buffer[0]))
+    weather = re.findall(r'[А-Я,а-я ]', str(buffer[0]))
     return ''.join(weather)
 
-def get_speed(html):
+def get_wind(html):
     buffer = html.find('div', id='hero-left-Nowcard-92c6937d-b8c3-4240-b06c-9da9a8b0d22b').find_all(class_='today_nowcard-sidecar component panel')
-    speed = re.findall(r'\d{1,2}', str(buffer[0]))
-    return speed[0]
+    wind = re.findall(r'[А-Я]{1,3} \d{1,2}', str(buffer[0]))[0]
+    wind = wind.split(' ')
+    return wind
 
-def get_date(temp, wether, speed):
-    return "Погода в Москве сейчас:\n" + str(temp) + "°, " + str(wether) + "\nВетер " + str(round(float(speed)/3.6, 2)) + " м/с"
+def get_date(temp, wether, wind):
+    wind_vector = wind[0]
+    wind_speed = str(round(float(wind[1])/3.6, 2))
+    return "Погода в Москве сейчас:\n" + str(temp) + "°, " + str(wether) + "\nВетер " + wind_vector + " " + wind_speed + " м/с"
 
 def weather():
     page = get_html(url)
     html = BeautifulSoup(page, 'lxml')
-    return get_date(get_temperature(html), get_weather(html), get_speed(html))
+    return get_date(get_temperature(html), get_weather(html), get_wind(html))
