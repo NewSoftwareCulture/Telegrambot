870dd47115a37089d92b218efef6b5b9c8963cff
Vlad <vlados.pochta@gmail.com>
2020-03-13 15:32:00 +0300

    Update url_Lushniki

--- actions/get_MoskowCentralRing.py
+++ actions/get_MoskowCentralRing.py
@@ -3,7 +3,7 @@ import re
 from bs4 import BeautifulSoup
 
 url_DelovoyCenter = 'https://t.rasp.yandex.ru/station/9855158/suburban/?direction=%D0%BF%D0%BE+%D1%87%D0%B0%D1%81%D0%BE%D0%B2%D0%BE%D0%B9+%D1%81%D1%82%D1%80%D0%B5%D0%BB%D0%BA%D0%B5+%28%D0%BD%D0%B0+%D0%94%D0%B5%D0%BB%D0%BE%D0%B2%D0%BE%D0%B9+%D1%86%D0%B5%D0%BD%D1%82%D1%80%29'
-url_Lushniki = 'https://t.rasp.yandex.ru/station/9855158/suburban/?direction=%D0%BF%D1%80%D0%BE%D1%82%D0%B8%D0%B2+%D1%87%D0%B0%D1%81%D0%BE%D0%B2%D0%BE%D0%B9+%D1%81%D1%82%D1%80%D0%B5%D0%BB%D0%BA%D0%B8+%28%D0%BD%D0%B0+%D0%90%D0%B2%D1%82%D0%BE%D0%B7%D0%B0%D0%B2%D0%BE%D0%B4%D1%81%D0%BA%D1%83%D1%8E%29'
+url_Lushniki = 'https://t.rasp.yandex.ru/station/9855158/'
 
 def get_html(url):
     r = requests.get(url)    
@@ -38,11 +38,11 @@ def get_table(html):
         i += 1
     return table
 
-def format(location, data):
+def format(location, header, table):
     i = 0
     result = location + '\n'
     while i < 5:
-        result += data[1][i] + ' (' + data[0][i] + ')'
+        result += table[i] + ' (' + header[i] + ')'
         if i < 4: result += '\n'
         i += 1
     return result
@@ -50,9 +50,9 @@ def format(location, data):
 
 def Lushniki():
     html = get_html(url_Lushniki)
-    return format('Лужники', [get_header(html), get_table(html)])
+    return format('Лужники', get_header(html), get_table(html))
 
 
 def DelovoyCenter():
     html = get_html(url_DelovoyCenter)
-    return format('Деловой центр', [get_header(html), get_table(html)])
+    return format('Деловой центр', get_header(html), get_table(html))
