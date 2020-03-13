53163cf8185afa17226736ba9ffd635e0bcd18cc
Vlad <vlados.pochta@gmail.com>
2020-03-13 16:38:46 +0300

    .

--- actions/get_MoskowCentralRing.py
+++ actions/get_MoskowCentralRing.py
@@ -3,7 +3,7 @@ import re
 from bs4 import BeautifulSoup
 
 url_DelovoyCenter = 'https://t.rasp.yandex.ru/station/9855158/suburban/?direction=%D0%BF%D0%BE+%D1%87%D0%B0%D1%81%D0%BE%D0%B2%D0%BE%D0%B9+%D1%81%D1%82%D1%80%D0%B5%D0%BB%D0%BA%D0%B5+%28%D0%BD%D0%B0+%D0%94%D0%B5%D0%BB%D0%BE%D0%B2%D0%BE%D0%B9+%D1%86%D0%B5%D0%BD%D1%82%D1%80%29'
-url_Lushniki = 'https://t.rasp.yandex.ru/station/9855158/'
+url_Lushniki = 'https://t.rasp.yandex.ru/station/9855158/suburban/?direction=%D0%BF%D1%80%D0%BE%D1%82%D0%B8%D0%B2+%D1%87%D0%B0%D1%81%D0%BE%D0%B2%D0%BE%D0%B9+%D1%81%D1%82%D1%80%D0%B5%D0%BB%D0%BA%D0%B8+%28%D0%BD%D0%B0+%D0%90%D0%B2%D1%82%D0%BE%D0%B7%D0%B0%D0%B2%D0%BE%D0%B4%D1%81%D0%BA%D1%83%D1%8E%29'
 
 def get_html(url):
     r = requests.get(url)    
