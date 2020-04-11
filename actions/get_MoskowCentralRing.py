c001c3ec05d4288c2d3663853da48c881f156c4e
Vlad <vlados.pochta@gmail.com>
2020-04-12 01:10:41 +0300

    Upd parser

--- actions/get_MoskowCentralRing.py
+++ actions/get_MoskowCentralRing.py
@@ -1,5 +1,6 @@
 import requests
 import re
+import time
 from bs4 import BeautifulSoup
 
 url_DelovoyCenter = 'https://t.rasp.yandex.ru/station/9855158/suburban/?direction=%D0%BF%D0%BE+%D1%87%D0%B0%D1%81%D0%BE%D0%B2%D0%BE%D0%B9+%D1%81%D1%82%D1%80%D0%B5%D0%BB%D0%BA%D0%B5+%28%D0%BD%D0%B0+%D0%94%D0%B5%D0%BB%D0%BE%D0%B2%D0%BE%D0%B9+%D1%86%D0%B5%D0%BD%D1%82%D1%80%29'
@@ -10,49 +11,26 @@ def get_html(url):
     r.encoding = 'utf8'
     return BeautifulSoup(r.text, 'lxml')
 
-def get_header(html):
-    header = ['' , '', '', '', '']
-    i = 0
-    while i < 5:
-        time = str(html.find_all(class_ = 'b-routers__time b-routers__time_type_before-departure-left')[i])
-        time = re.findall(r'\d{1,2}', time)
-        if len(time) == 2:
-            header[i] = 'через ' + time[0] + ' ч. ' + time[1] + ' мин.'
-        else:
-            header[i] = 'через ' + time[0] + ' минут'
-        i += 1
-    return header
-
 def get_table(html):
     table = ['', '', '', '', '']
-    number = 0
-    month = ''
-    i = 0
-    while i < 5:
-        buffer = str(html.find_all(class_ = 'b-routers__time b-routers__time_type_departure')[i])
-        if i == 0: 
-            number = str(re.findall(r'\d{1,2}', buffer)[0])
-            month = ''.join(re.findall(r'[а-я]', buffer))
-        time = str(re.findall(r'\d\d:\d\d', buffer)[0])
-        table[i] = number + ' ' + month + '  -  ' + time
-        i += 1
+    buffer = str(html.find_all('table')[0].find_all(class_ = 'StationTable__presentThreads'))
+    times = re.findall(r'\d\d:\d\d', buffer)
+    date = re.findall(r'\d{1,2} [А-я]*.', buffer)[0]
+    for i in range(5):
+        table[i] = date + '  -  ' + times[i]
     return table
 
-def format(location, header, table):
-    i = 0
+def format(location, line):
     result = location + '\n'
-    while i < 5:
-        result += table[i] + ' (' + header[i] + ')'
-        if i < 4: result += '\n'
-        i += 1
-    return result
-
+    for i in line:
+        result += i + '\n' 
+    return result[:-1]
 
 def Lushniki():
     html = get_html(url_Lushniki)
-    return format('Лужники', get_header(html), get_table(html))
+    return format('Лужники', get_table(html))
 
 
 def DelovoyCenter():
     html = get_html(url_DelovoyCenter)
-    return format('Деловой центр', get_header(html), get_table(html))
+    return format('Деловой центр', get_table(html))
